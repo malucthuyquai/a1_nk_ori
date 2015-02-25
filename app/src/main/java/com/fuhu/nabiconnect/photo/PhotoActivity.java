b02ffdedcb7edbc4a93b6ee240d3f74b010d9073
endor_ <chris.hsu@fuhu.com>
2015-02-25 15:01:50 +0800

    hide all views in photo act until login succeeds

--- app/src/main/java/com/fuhu/nabiconnect/photo/PhotoActivity.java
+++ app/src/main/java/com/fuhu/nabiconnect/photo/PhotoActivity.java
@@ -16,6 +16,7 @@ import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.androidquery.AQuery;
 import com.fuhu.data.UserData;
 import com.fuhu.nabiconnect.R;
 import com.fuhu.nabiconnect.application.NabiconnectApplication;
@@ -85,6 +86,8 @@ public class PhotoActivity extends ApiBaseActivity {
     private PhotoMyGalleryFragment mygalleryFragment = null;
     private PhotoCameraGalleryFragment cameragalleryFragment = null;
 
+    private AQuery aq;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
@@ -128,10 +131,15 @@ public class PhotoActivity extends ApiBaseActivity {
 
         SetLastButtonBarViewAndClickListener();
 
+        /**
+         * hide all views until login returns
+         */
+        aq = new AQuery(PhotoActivity.this);
+        hideAllView();
+
         CheckWifi();
         CheckMode();
         CheckModeIcon();
-
     }
 
     /*
@@ -140,38 +148,30 @@ public class PhotoActivity extends ApiBaseActivity {
      * =======================================================================
      */
     protected boolean isWifiEnabled() {
-
         NetworkInfo info = this.m_Connectivity.getActiveNetworkInfo();
         if (info == null || !this.m_Connectivity.getBackgroundDataSetting()) {
-
             LOG.I(TAG, "No Wifi");
             return false;
 
         }
-
         int netType = info.getType();
         if (netType == ConnectivityManager.TYPE_WIFI) {
             LOG.I(TAG, "have Wifi");
             return info.isConnected();
         }
-
         return false;
     }
 
     protected void StartWifiSetDialog() {
-
         Intent intent = new Intent(INTENT_CONNECTION_DIALOG);
         sendBroadcast(intent);
-
     }
 
     public void CheckWifi() {
-
         boolean isWifi = isWifiEnabled(); // check WIFI
         LOG.I(TAG, "isWifi = " + isWifi);
         if (!isWifi)
             StartWifiSetDialog();
-
     }
 
     protected void CheckMode() {
@@ -184,7 +184,6 @@ public class PhotoActivity extends ApiBaseActivity {
                 LOG.V(TAG, "CheckMode() - there is logon userkey. Get data from cache.");
                 UserData logonUser = getDatabaseAdapter().getUserData(
                         m_LogonUserKey);
-
                 if (logonUser != null) {
                     LOG.V(TAG, "CheckMode() - use the cache as the logon data.");
                     loginUserSuccess(logonUser);
@@ -198,12 +197,10 @@ public class PhotoActivity extends ApiBaseActivity {
             }
         } else
             loginAccount(); // for get user information
-
         LOG.V(TAG, "IsMommyMode = " + IsMommyMode);
     }
 
     private void CheckModeIcon() {
-
         if (IsMommyMode) {
             NabiPhotoIcon.setBackgroundResource(R.drawable.connect_bar_icon);
         } else {
@@ -221,17 +218,14 @@ public class PhotoActivity extends ApiBaseActivity {
         transaction.addToBackStack(tag);
         transaction.replace(R.id.photo_fragment_container, fragclass, tag);
         transaction.commit();
-
     }
 
     public void switchFragment(Fragment fragclass, Bundle bundle, String tag) {
-
         fragclass.setArguments(bundle);
         transaction = mFragmentManager.beginTransaction();
         transaction.addToBackStack(tag);
         transaction.replace(R.id.photo_fragment_container, fragclass, tag);
         transaction.commit();
-
     }
 
     public String getUserId() {
@@ -266,39 +260,31 @@ public class PhotoActivity extends ApiBaseActivity {
 
     @Override
     public void loginUserSuccess(UserData data) {
-        // TODO Auto-generated method stub
         super.loginUserSuccess(data);
-
+        showAllView();
         UserId = this.getCurrentUserData().userKey;
         UserName = this.getCurrentUserData().userName;
         UserAvatarUrl = this.getCurrentUserData().avatarURL;
-
         LOG.I(TAG, " action = " + ActionName);
         if (gotoCameraGalleryFragmentForWingsChallenge(ActionName)) {
-
             if (cameragalleryFragment == null) {
                 CameraBtn.setSelected(true);
                 SetLastButtonEnable(false);
                 cameragalleryFragment = new PhotoCameraGalleryFragment();
                 switchFragment(cameragalleryFragment, PhotoParameter.FRAGMENTTAG_CAMERAGALLERY);
             }
-
         } else {
-
             getFriendList(UserId);
-
             if (InboxFragment == null) {
                 HomeInboxBtn.setSelected(true);
                 InboxFragment = new PhotoInBoxFragment();
                 switchFragment(InboxFragment, PhotoParameter.FRAGMENTTAG_INBOX);
             }
         }
-
     }
 
     @Override
     public void loginUserFailure(String data) {
-        // TODO Auto-generated method stub
         super.loginUserFailure(data);
         LOG.E(TAG, "login failure !!");
         showGeneralWarningDialog();
@@ -307,7 +293,6 @@ public class PhotoActivity extends ApiBaseActivity {
 
     @Override
     public void getFriendListSuccess(friends_outObj data) {
-        // TODO Auto-generated method stub
         super.getFriendListSuccess(data);
         LOG.V(TAG, "GetFriendListSuccess!!  total = " + data.getFriends().size());
         UserFriendsCount = data.getFriends().size();
@@ -316,7 +301,6 @@ public class PhotoActivity extends ApiBaseActivity {
 
     @Override
     public void getFriendListFailure(String data) {
-        // TODO Auto-generated method stub
         super.getFriendListFailure(data);
         LOG.E(TAG, "get friendlist failure !!");
         showGeneralWarningDialog();
@@ -336,8 +320,6 @@ public class PhotoActivity extends ApiBaseActivity {
     }
 
     protected void SetLastButtonBarViewAndClickListener() {
-        // TODO Auto-generated method stub
-
         NabiPhotoIcon = (ImageView) findViewById(R.id.NabiPhotoIcon);
         HomeInboxBtn = (ImageButton) findViewById(R.id.HomeInboxButton);
         CameraBtn = (ImageButton) findViewById(R.id.CameraButton);
@@ -347,7 +329,6 @@ public class PhotoActivity extends ApiBaseActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
 
 				/*
                  * boolean nowmode = mode.getBoolean("MODE", false); String
@@ -364,7 +345,6 @@ public class PhotoActivity extends ApiBaseActivity {
                 CameraBtn.setSelected(false);
                 MyGalleryBtn.setSelected(false);
 
-
                 LOG.W(TAG, "mFragmentManager.getBackStackEntryCount() = " + mFragmentManager.getBackStackEntryCount());
                 if (mFragmentManager.getBackStackEntryCount() > 1) {
                     mFragmentManager.popBackStack(PhotoParameter.FRAGMENTTAG_INBOX, 0);
@@ -378,16 +358,12 @@ public class PhotoActivity extends ApiBaseActivity {
                             LOG.I(TAG, "InboxFragment isadded !! ");
                             return;
                         }
-
                         switchFragment(InboxFragment, PhotoParameter.FRAGMENTTAG_INBOX);
                     } else {
-
                         InboxFragment = new PhotoInBoxFragment();
                         switchFragment(InboxFragment, PhotoParameter.FRAGMENTTAG_INBOX);
-
                     }
                 }
-
             }
         });
 
@@ -395,8 +371,6 @@ public class PhotoActivity extends ApiBaseActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
 				/*
                  * boolean nowmode = mode.getBoolean("MODE", false); String
 				 * logonUserKey = mode.getString("LOGON_USER_KEY", "");
@@ -407,11 +381,9 @@ public class PhotoActivity extends ApiBaseActivity {
 				 * nowmode); intent.putExtra(KEY_LOGON_USER_KEY, logonUserKey);
 				 * startActivity(intent);
 				 */
-
                 HomeInboxBtn.setSelected(false);
                 CameraBtn.setSelected(true);
                 MyGalleryBtn.setSelected(false);
-
 
                 LOG.W(TAG, "mFragmentManager.getBackStackEntryCount() = " + mFragmentManager.getBackStackEntryCount());
                 if (mFragmentManager.getBackStackEntryCount() > 2 && cameraFragment != null) {
@@ -425,16 +397,12 @@ public class PhotoActivity extends ApiBaseActivity {
                             LOG.I(TAG, "cameraFragment isadded !! ");
                             return;
                         }
-
                         switchFragment(cameraFragment, PhotoParameter.FRAGMENTTAG_CAMERA);
                     } else {
-
                         cameraFragment = new PhotoCameraFragment();
                         switchFragment(cameraFragment, PhotoParameter.FRAGMENTTAG_CAMERA);
-
                     }
                 }
-
             }
         });
 
@@ -442,8 +410,6 @@ public class PhotoActivity extends ApiBaseActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
 				/*
                  * boolean nowmode = mode.getBoolean("MODE", false); String
 				 * logonUserKey = mode.getString("LOGON_USER_KEY", "");
@@ -460,7 +426,6 @@ public class PhotoActivity extends ApiBaseActivity {
                 CameraBtn.setSelected(false);
                 MyGalleryBtn.setSelected(true);
 
-
                 LOG.W(TAG, "mFragmentManager.getBackStackEntryCount() = " + mFragmentManager.getBackStackEntryCount());
                 if (mFragmentManager.getBackStackEntryCount() > 2 && mygalleryFragment != null) {
                     mFragmentManager.popBackStack(PhotoParameter.FRAGMENTTAG_MYGALLERY, 0);
@@ -474,41 +439,41 @@ public class PhotoActivity extends ApiBaseActivity {
                         }
                         switchFragment(mygalleryFragment, PhotoParameter.FRAGMENTTAG_MYGALLERY);
                     } else {
-
                         mygalleryFragment = new PhotoMyGalleryFragment();
                         switchFragment(mygalleryFragment, PhotoParameter.FRAGMENTTAG_MYGALLERY);
-
                     }
                 }
-
             }
-
         });
     }
 
     @Override
     public void onBackPressed() {
-        // TODO Auto-generated method stub
-
         LOG.W(TAG, "click backbtn = " + mFragmentManager.getBackStackEntryCount());
         if (mFragmentManager.getBackStackEntryCount() == 1) {
             finish();
         } else {
             super.onBackPressed();
         }
-
     }
 
     @Override
     protected void onDestroy() {
-        // TODO Auto-generated method stub
         super.onDestroy();
         LOG.I(TAG, "onDestroy");
-
         InboxFragment = null;
         cameraFragment = null;
         mygalleryFragment = null;
         //cleanCacheMap();
     }
 
+    private void hideAllView() {
+        aq.id(R.id.LastButtonBarWidget).invisible();
+        aq.id(R.id.photo_fragment_container).invisible();
+    }
+
+    private void showAllView() {
+        aq.id(R.id.LastButtonBarWidget).visible();
+        aq.id(R.id.photo_fragment_container).visible();
+    }
 }
